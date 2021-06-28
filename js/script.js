613b37d580883b5efe8af34b639f995ba9d32c5f
Marek Witkowski <marek.witkowski96@icloud.com>
2021-06-28 13:02:48 +0200

    Update all files

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-function
-	let computerMove ='kamień';
-	let playerMove ='papier';
- 
-	printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to' + playerMove +', to wygrywasz!'){
+function printMessage(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
