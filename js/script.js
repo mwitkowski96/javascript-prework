45240c0fc8d6126db01fa3e877a104a758bf674a
Marek Witkowski <marek.witkowski96@icloud.com>
2021-06-28 14:33:31 +0200

    Zadanie z wstawianiem tekstu

--- js/script.js
+++ js/script.js
@@ -8,62 +8,8 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let computerMove = `kamień`;
+let playerMove = 'papier';
 
-console.log('Gracz wpisał: ' + playerInput);
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ' ,to wygrywasz!');
 
-let playerMove = 'nieznany ruch';
-
-
-if(playerInput == '1'){
-  playerMove = 'kamień';
-} else if(playerInput == '2') {
-playerMove = 'papier';
-} else {
-playerMove = 'nożyce'
-}
-
-printMessage('Twój ruch to: ' + playerMove);
-
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-console.log('Wylosowana liczba to: ' + randomNumber);
-
-let computerMove = 'nieznany ruch';
-
-if(randomNumber == 1){
-  computerMove = 'kamień';
-} else if(randomNumber ==2){
-computerMove = 'papier';
-}else{
-computerMove = 'nożyce';
-}
-
-printMessage('Mój ruch to: ' + computerMove);
-  
-
-
-
-if( computerMove == 'kamień' && playerMove == 'papier'){
-printMessage('Ty wygrywasz!');
-} else if( computerMove == 'kamień' &&  playerMove == 'nożyce'){
-printMessage('Przegrywasz!');
-} else if (computerMove =='kamień' && playerMove =='kamień'){
-printMessage('Remis!');
-}
-
-if( computerMove == 'papier' && playerMove == 'nożyce'){
-printMessage('Ty wygrywasz!');
-} else if( computerMove == 'papier' && playerMove == 'kamień'){
-printMessage('Przegrywasz!');
-} else if (computerMove =='papier' && playerMove =='papier'){
-printMessage('Remis!')
-}
-
-if( computerMove == 'nożyce' && playerMove == 'kamień'){
-printMessage('Ty wygrywasz!');
-} else if ( computerMove == 'nożyce' && playerMove == 'papier'){
-printMessage('Przegrywasz!');
-} else if (computerMove =='nożyce' && playerMove =='nożyce'){
-printMessage('Remis!');
-}
